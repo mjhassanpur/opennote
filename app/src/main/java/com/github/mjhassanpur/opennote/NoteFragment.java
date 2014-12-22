fea1cb16b2e07d1a975c9eab2da56ca4d529179c
mjhassanpur <mjhassanpur@gmail.com>
2014-12-22 12:50:13 -0800

    Update note layout

--- app/src/main/java/com/github/mjhassanpur/opennote/NoteFragment.java
+++ app/src/main/java/com/github/mjhassanpur/opennote/NoteFragment.java
@@ -7,6 +7,10 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class NoteFragment extends Fragment {
@@ -56,7 +60,30 @@ public class NoteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note, container, false);
+        View v = inflater.inflate(R.layout.fragment_note, container, false);
+
+        final TextView tv = (TextView) v.findViewById(R.id.view_note);
+
+        final EditText et = (EditText) v.findViewById(R.id.add_note);
+        if (et != null) {
+            et.setHorizontallyScrolling(false);
+            et.setLines(5);
+        }
+        et.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
+                int result = actionId & EditorInfo.IME_MASK_ACTION;
+                switch (result) {
+                    case EditorInfo.IME_ACTION_DONE:
+                        tv.setText(et.getText().toString());
+                        et.getText().clear();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
